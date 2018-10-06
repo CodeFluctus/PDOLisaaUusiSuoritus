1dd3ebfe40ee857f87176928c2e00db5b6379c54
CodeFluctus <t7aaju00@students.oamk.fi>
2018-10-06 22:09:18 +0300

    Poistettu ylimääräiset kommentoidut tekstit

--- dbInsertUusiSuoritus.php
+++ dbInsertUusiSuoritus.php
@@ -1,37 +1,4 @@
 <?php
-
-
-/* TOIMIIIII!
-try {
-    $yhteys = new PDO("mysql:host=localhost;dbname=t7aaju00", "t7aaju00",
-   "salasana");
-   } catch (PDOException $e) {
-    die("VIRHE: " . $e->getMessage());
-   }
-   // virheenkäsittely: virheet aiheuttavat poikkeuksen
-   $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   // merkistö: käytetään latin1-merkistöä; toinen yleinen vaihtoehto on utf8.
-   $yhteys->exec("SET NAMES latin1");
-   $kysely = $yhteys->prepare("INSERT INTO Opiskelija
-   (Etunimi,Sukunimi,Osoite,Luokkatunnus) VALUES (?, ?, ?, ?)");
-   $kysely->execute(array($_POST["etun"], $_POST["sukun"],$_POST["as"],
-   $_POST["oj"] ));
-   //Yhteyden poistaminen. Ei välttämättä tarvita
-   $yhteys = null;
-
-   
-   
-
-
-
-
-
-
-    $host = 'localhost';
-    $dbname = 't7aaju00';
-    $username = 't7aaju00';
-    $password = 'salasana';*/
-
     try
     {
         $yhteys = new PDO("mysql: host=localhost; dbname=t7aaju00", "t7aaju00", "salasana");
@@ -42,16 +9,13 @@ try {
     }
 
     //Virheenkäsittely: virheet aiheuttavat poikkeuksen
-
     $yhteys -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     //Merkistö: Käytetään latin1-merkistöä.(utf8 on myös yleinen)
     $yhteys ->exec ("SET NAMES latin1");
 
     //valmistellaan proseduuri
-
     $proseduuri = $yhteys->prepare("CALL UusiSuoritus(?, ?, ?, ?)");
-    //$proseduuri =$yhteys -> prepare("CALL UusiSuoritus($_POST["etun"], $_POST["sukun"], $_POST["oj"], $_POST["as"])";
 
     //Suoritetaan valmisteltu proserduuri
 
@@ -63,34 +27,5 @@ try {
     echo "Merkintä tietokantaan suoritettu onnistuneesti!";
     echo "<br>";
     echo "Palaa selaimen edellinen-painikkeella takaisin!";
-
-
-    //------------------------------------------
-
-    /*
-    $con = mysqli_connect($host, $username, $password, $dbname);
-
-    if (mysqli_connect_errno())
-    {
-        echo "Failed to connect MySQL! Error: " . myslqi_connect_error(); 
-    }
-
-    $etunimi = mysqli_real_escape_string($con, filter_input(INPUT_POST,'etun',FILTER_SANITIZE_STRING));
-    $sukunimi = mysqli_real_escape_string($con, filter_input(INPUT_POST,'sukun',FILTER_SANITIZE_STRING));
-    $opintojaksonKoodi = mysqli_real_escape_string($con,filter_input(INPUT_POST,'oj',FILTER_SANITIZE_STRING));
-    $arvosana = mysqli_real_escape_string($con, filter_input(INPUT_POST,'as',FILTER_SANITIZE_STRING));
-
-    $sql = "CALL UusiSuoritus('$etunimi', '$sukunimi', '$opintojaksonKoodi', $arvosana)";
-
-    if (!mysqli_query($con, $sql))
-    {
-        die('Error: ' . mysqli_error($con));
-    }
-        
-    echo "Merkintä tietokantaan suoritettu onnistuneesti!";
-    echo "<br>";
-    echo "Palaa selaimen edellinen-painikkeella takaisin!";
-
-    mysqli_close($con);*/
 
 ?>
